996bc2bfa005ca0ac921305d7d6fcac0844aca14
Lili <lidiaechuacas@hotmail.com>
2023-01-19 17:56:21 +0100

    19/01

--- ActividadLogin/Controllers/HomeController.cs
+++ ActividadLogin/Controllers/HomeController.cs
@@ -6,22 +6,11 @@ namespace ActividadLogin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
+        
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
