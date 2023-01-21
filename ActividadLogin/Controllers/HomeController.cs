671f3e68e1585508aff5ccbdc0576ade72e1bf9e
Lili <lidiaechuacas@hotmail.com>
2023-01-21 17:49:39 +0100

    arreglao

--- ActividadLogin/Controllers/HomeController.cs
+++ ActividadLogin/Controllers/HomeController.cs
@@ -2,30 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-
 namespace ActividadLogin.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+           return View();
         }
-        //public ActionResult loginformu(FormCollection credenciales)
-        //{
-        //    string usuario = credenciales.Get("angel");
-        //    string contra = credenciales.Get("calculadora");
-        //    if (usuario == "angel" && contra == "calculadora")
-        //    {
-        //        //mensaje = "has iniciado sesion correctamente c:";
-        //    }
-        //    else
-        //    {
-        //        //mensaje = "nombre de usuario y/o contraseña incorrecta :c";
 
-        //    }
-
-        //    return View("Index");
+        [HttpPost]
+        public ActionResult Login(UsuarioModel usuarioModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Mensaje = "Dios Cris te como la boca <3";
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.Mensaje = "Ta mal";
+                return View("Index", usuarioModel);
+            }
 
         }
+    }
 }
---
+++ ActividadLogin/Models/UsuarioModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ActividadLogin.Models
+{   
+    public class UsuarioModel
+    {
+        [Display(Name = "usuario")]
+        [Required(ErrorMessage = "tienes que poner un email válido :cc")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "losiento correo electronico pocho")]
+        public string usuario { get; set; }
+
+        [Display(Name = "contra")]
+        [Required(ErrorMessage = "contraseña no válida :cc")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z]).{8,}$", ErrorMessage = "losiento contraseña pocha")]
+        public string contra { get; set; }
+
+        
+
+    }
+}
